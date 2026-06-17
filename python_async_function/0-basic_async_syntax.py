88a91ea903dd9ef75a2cac0c7afda79d8fcdd797
Litoxam <litobernier@gmail.com>
2026-06-17 12:21:20 +0200

    waits for a random delay between 0 and max_delay

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""The basics of async"""
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay=10):
+    """Wait for a random delay and return it."""
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
+
+if __name__ == "__main__":
+    print(asyncio.run(wait_random()))
+    print(asyncio.run(wait_random(5)))
+    print(asyncio.run(wait_random(15)))
