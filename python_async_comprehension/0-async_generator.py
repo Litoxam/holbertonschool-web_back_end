8cf4330df5eadfbe4f52ddba2f6c4fab3c1adb2b
Litoxam <litobernier@gmail.com>
2026-06-18 14:39:59 +0200

    Generator float

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,7 +5,7 @@ import asyncio
 import random
 
 
-async def async_generator() -> Generator[int, None, None] :
+async def async_generator() -> Generator[float, None, None]:
     """Create a async Generator, yield random int 10 times"""
     for _ in range(10):
         await asyncio.sleep(1)
