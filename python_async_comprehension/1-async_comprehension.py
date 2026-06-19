2de93c2e6f2b263b8b765f197801c391fa32a2f1
Litoxam <litobernier@gmail.com>
2026-06-19 16:23:17 +0200

    fix: use of another syntaxe for the return

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -7,10 +7,7 @@ async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> List[float]:
     """Collect 10 random numbers using an async comprehension"""
-    result = []
-    async for i in async_generator():
-        result.append(i)
-    return result
+    return [i async for i in async_generator()]
 
 if __name__ == "__main__":
     async def main():
