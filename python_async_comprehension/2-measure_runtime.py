8be2ccb11831823fe2397587feaa4ec8ebc97f6e
Litoxam <litobernier@gmail.com>
2026-06-19 16:29:16 +0200

    Execute four async comprehensions in parallel and return the runtime

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Async comprehension"""
+import asyncio
+from typing import List
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """Execute four async comprehensions in parallel and return the runtime."""
+    start = time.perf_counter()
+
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
+    )
+
+    end = time.perf_counter()
+    return end - start
+
+if __name__ == "__main__":
+    async def main():
+        return await (measure_runtime())
+
+    print(
+        asyncio.run(main())
+    )
